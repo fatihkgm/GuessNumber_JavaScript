8ad5feb157611a3f0d95df483023fbf98b6d69dc
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-02-24 00:41:02 -0500

    basic opeartion implemented

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+/*
+document.querySelector('.message').textContent = '📌 Correct Number';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+
+*/
+
